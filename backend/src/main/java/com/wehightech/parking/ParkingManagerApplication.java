a45ad9d584198b5b671023f8bae9c4fffb202802
Riadh MNASRI <riadh.mnasri@gmail.com>
2022-05-12 01:01:24 +0200

    feat(parking-manager) Setup spring code base

---
+++ backend/src/main/java/com/wehightech/parking/ParkingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wehightech.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagerApplication.class, args);
+	}
+
+}
